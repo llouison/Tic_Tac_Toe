e6d6c5bf846cb4ae92077241175c8fd3abaa1d79
lisa louison <lisa.louison@gmail.com>
2017-04-09 15:02:23 -0400

    added click event listeners to each div

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 console.log('connected!');
 
+//creating a variable to locate all the h2s
+const whichDiv = document.querySelectorAll('.turn');
+//creating a for loop to loop through the h2s
+for (let i = 0; i < whichDiv.length; i++) {
+    console.log(whichDiv[i]);
+    //adding an eventlistener to each h2 that executes the enterplay function when clicked
+    whichDiv[i].addEventListener('click', enterPlay);
+    //the h2s need content to recognize the click. Adding z to their innerhtml.
+    whichDiv[i].innerHTML = 'z';
+    //declaring the enterplay function
+    function enterPlay(){
+            //calling the whosTurn function
+            whosTurn();
+            console.log(i);
+            //removing the event listener once the h2 is clicked
+             whichDiv[i].removeEventListener('click', enterPlay);
+    }
+}      
+
+
+
 function clearText(){
     //locating all the text elements
     const allText = document.querySelectorAll('.text');
@@ -9,6 +30,7 @@ function clearText(){
         allText[i].innerHTML = '';
     }
 }
+
 
 window.onload = function() {
     //setting an event listener to clear all the text when the restart button is clicked
