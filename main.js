9028d88262384cd326a252794cbdec32e1c89a06
lisa louison <lisa.louison@gmail.com>
2017-04-09 15:41:27 -0400

    increment the turn to change the input value in js

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 console.log('connected!');
 
+
+
 //creating a variable to locate all the h2s
 const whichDiv = document.querySelectorAll('.turn');
+ let turn = 0;
 //creating a for loop to loop through the h2s
-for (let i = 0; i < whichDiv.length; i++) {
-    console.log(whichDiv[i]);
+for (let i = 0; i < whichDiv.length; i++) { 
     //adding an eventlistener to each h2 that executes the enterplay function when clicked
     whichDiv[i].addEventListener('click', enterPlay);
-    //the h2s need content to recognize the click. Adding z to their innerhtml.
-    whichDiv[i].innerHTML = 'z';
+    //the h2s need content to recognize the click
+    whichDiv[i].innerHTML = '&hellip;';
     //declaring the enterplay function
     function enterPlay(){
-            //calling the whosTurn function
-            whosTurn();
-            console.log(i);
-            //removing the event listener once the h2 is clicked
-             whichDiv[i].removeEventListener('click', enterPlay);
+        //incrementing the turn variable by 1
+        turn ++;
+        //creating an if statement to change the h2s innerhtml to 'o' if the turn is even
+        if(turn % 2 === 0){  
+            whichDiv[i].innerHTML = 'o';
+        }
+        //if the turn is odd, the else statement changes the h2s innerhtml to 'o' 
+        else {
+            whichDiv[i].innerHTML = 'x';
+        }
+        //removing the event listener once the h2 is clicked
+        whichDiv[i].removeEventListener('click', enterPlay);
     }
 }      
 
